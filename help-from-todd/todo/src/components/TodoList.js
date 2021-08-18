8937f83b5f3791a76abc834873e90463cae9bf31
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-08-17 23:11:07 -0700

    Finally

---
+++ help-from-todd/todo/src/components/TodoList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function TodoList(props) {
+    return (
+        <div>
+            <h1>List of Todos</h1>
+            {props.data.map(function (todo, index) {
+
+                //console.log(todo.isCompleted)
+
+                return (
+                    <div key={index}>
+                        <input type='checkbox' checked={todo.isCompleted} onChange={() => props.completeTodo(todo.id)}></input>
+                        <label
+                            onClick={() => props.completeTodo(todo.id)}
+                            className={todo.isCompleted ? 'todo-title-completed' : 'todo-title'}>
+                            {todo.text}
+                        </label>
+                        <button onClick={() => props.deleteTodo(todo.id)}>X</button>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default TodoList
