e4037ceb20daf47d93956fb494678c6cac0324b3
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-08-18 12:20:41 -0700

    Todo emded TodoList js line 20

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+// import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Todo App</h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
